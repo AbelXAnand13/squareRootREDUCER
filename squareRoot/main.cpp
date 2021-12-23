fe5a0b8d72112bd7e399fb78eded82e725336caa
/\be| /\nand <90404118+AbelXAnand13@users.noreply.github.com>
2021-12-23 12:00:26 -0800

    Im in tears rn :(

--- squareRoot/main.cpp
+++ squareRoot/main.cpp
@@ -1,14 +1,24 @@
-//
-//  main.cpp
-//  squareRoot
-//
-//  Created by Abel Christy Anand on 12/14/21.
-//
-
+// Abel Anand
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+void sqrtReducer(int num) {
+    int primeNum = 1;
+    int d = 2;
+    while (d * d <= num){
+    if (num % (d * d) == 0){
+    num = num / (d * d);
+    primeNum = primeNum * d;
+    }
+    else{
+          d = d + 1;
+    }
+        
+}
+    cout << primeNum<< " * sqrt" << num;
+}
+int main() {
+    int num;
+    cout << "Enter a number to reduce\n";
+    cin >> num;
+    sqrtReducer(num);
 }
